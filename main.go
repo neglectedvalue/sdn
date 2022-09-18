c241bbcde61e1900318fcfebba51a7e2a3cf38d6
ngv <funkycatz@yandex.ru>
2022-09-18 23:04:57 +0300

    redis added

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/cache/v8"
+	"github.com/go-redis/redis/v8"
 )
 
 func main() {
@@ -12,11 +15,29 @@ func main() {
 		port = "3000"
 	}
 
+	redisURL := os.Getenv("REDIS_URL")
+	if len(redisURL) == 0 {
+		redisURL = "redis://:@localhost:6370/1"
+	}
+
+	redisOptions, err := redis.ParseURL(redisURL)
+	if err != nil {
+		panic(err)
+	}
+	redisClient := redis.NewClient(redisOptions)
+	defer redisClient.Close()
+	redisCache := cache.New(&cache.Options{
+		Redis: redisClient,
+	})
+	server := &Server{
+		RedisCache: redisCache,
+	}
+
 	addr := ":" + port
 
 	fmt.Printf("-= Starting web server, listening on %s =-\n", addr)
 
-	err := http.ListenAndServe(addr, &Server{})
+	err = http.ListenAndServe(addr, server)
 	if err != nil {
 		panic(err)
 	}
